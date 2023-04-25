a400a438413ac67b7fb8fead3603b717e28c1ab8
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-04-25 15:28:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/94c74580a918cb9e6b18e.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
